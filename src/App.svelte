2cbd2e61d4a6a7d0fdb293bde464b34ad6be87bc
ylor <rolyreyes@me.com>
2020-03-22 16:13:31 -0400

    update bloat.js

--- src/App.svelte
+++ src/App.svelte
@@ -5,19 +5,6 @@
   import Search from "./components/Search.svelte";
 
   import keyHandler from "./js/keyHandler.js";
-
-  // const work = location.href.includes("work" | "localhost") ? true : false;
-  // console.log("work", work);
-
-  function escHandler(event) {
-    // Listen for esc
-    if (event.key === "Escape") {
-      // If search-input is focused and has a value, zero it out
-      if (search.length > 0) {
-        search = "";
-      }
-    }
-  }
 </script>
 
 <svelte:window on:keydown={keyHandler} />
